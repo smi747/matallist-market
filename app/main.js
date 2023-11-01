a5ef041cefc6e70119d6ce2eebb68ae523cb4bd2
Michael Sladkov <sabpp@mail.ru>
2023-11-01 23:42:32 +0300

    working slider

--- app/main.js
+++ app/main.js
@@ -19,3 +19,26 @@ function bringmenu() {
   }
   }
   
+  
+  var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    initialSlide: 2,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
