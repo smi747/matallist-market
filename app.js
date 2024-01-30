ec588758eb6b1de4bb8331aa83bc5a1369cd4748
Michael Sladkov <sabpp@mail.ru>
2024-01-30 19:44:12 +0300

    mysql wip 1

--- app.js
+++ app.js
@@ -193,7 +193,13 @@ app.post("/admin", checkAuthenticated, multer({ storage: storageConfig }).single
         if (code !== 0) {
         console.log(`grep process exited with code ${code}`);
         }
-        res.send(`completed`);
+        const command_2 = spawn('sudo', ["cp", "GFG", "var/lib/mysql/positions"]);
+        command_2.on('close', (code) => {
+            if (code !== 0) {
+            console.log(`grep process exited with code ${code}`);
+            }
+            const command_3 = spawn('mysql', ["-padmin", "<", "musqlscript"]);
+            res.send(`completed`);})
     });
 });
 
