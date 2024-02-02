2d58e31bfe8528f16e4d30a5deb468e2a01b063d
Michael Sladkov <sabpp@mail.ru>
2024-02-03 01:58:40 +0300

    empty dir fix 2

--- app.js
+++ app.js
@@ -374,10 +374,11 @@ res.send({ express: JSON.stringify(tmp)})
 app.get('/get_photos', (req, res) => {
     let tmp = _getAllFilesFromFolder("app/images/slider");
     let result = [];
-    if (!fs.existsSync("app/images/slider/"+x)){
-        fs.mkdirSync("app/images/slider/"+x);
-    }
-    tmp.map(x => result.push(_getAllFilesFromFolder("app/images/slider/"+x)));
+    
+    tmp.map(x => {
+        if (!fs.existsSync("app/images/slider/"+x)){
+            fs.mkdirSync("app/images/slider/"+x);
+        };result.push(_getAllFilesFromFolder("app/images/slider/"+x))});
     res.send({ express: JSON.stringify(result)})
     });
 
